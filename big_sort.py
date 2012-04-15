a5e7e63dbdcb98e4e79800101bf642d5f813f916
Steve Howell <steve@strings.com>
2012-04-15 11:17:07 -0700

    multi level sort

--- big_sort.py
+++ big_sort.py
@@ -14,11 +14,16 @@ def make_tree(max, fn, elements):
     for item in elements:
         f.write(str(item) + '\n')
     f.close()
+    if elements:
+        head = elements[0]
+    else:
+        head = None
     return {
         'fn': fn,
         'cnt': 0,
         'children': None,
         'max': max,
+        'head': head
     }
 
 def add_to_tree(tree, item):
@@ -28,8 +33,17 @@ def add_to_tree(tree, item):
     else:
         add_to_simple_tree(tree, item)
 
+def get_position(children, item):
+    last = len(children) - 1
+    for i in range(last):
+        head = children[i+1]['head']
+        if item < head:
+            return i
+    return last
+
 def add_to_parent_tree(tree, children, item):
-    pass
+    pos = get_position(children, item)
+    add_to_tree(children[pos], item)
 
 def add_to_simple_tree(tree, item):
     max = tree['max']
@@ -40,11 +54,11 @@ def add_to_simple_tree(tree, item):
     tree['cnt'] += 1
     cnt = tree['cnt']
     if cnt >= max:
-        split_tree(tree, cnt)
+        split_tree(tree, max, cnt)
 
-def split_tree(tree, cnt):
+def split_tree(tree, max, cnt):
     fn = tree['fn']
-    elements = [line.strip() for line in open(fn)]
+    elements = [int(line.strip()) for line in open(fn)]
     elements.sort()
     m = (cnt + 1) // 2
     fn_0 = fn + '_0'
@@ -69,7 +83,7 @@ def print_tree(tree):
             print item
 
 idx = make_root_tree(10, '/tmp/foo')
-for i in range(11):
+for i in range(35):
     n = random.randint(1, 1000)
     add_to_tree(idx, n)
 print_tree(idx)
