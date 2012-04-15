3797596c279eb894fa7723bdbfa033fdc61758fe
Steve Howell <steve@strings.com>
2012-04-15 10:36:42 -0700

    splitting

--- big_sort.py
+++ big_sort.py
@@ -15,16 +15,36 @@ def make_index(max, elements = None):
     }
 
 def add_to_index(index, item):
-    index['elements'].append(item)
-    if len(index['elements']) >= index['max']:
-        index['elements'].sort()
+    add_to_simple_index(index, item)
+
+def add_to_simple_index(index, item):
+    max = index['max']
+    elements = index['elements']
+    elements.append(item)
+    cnt = len(elements)
+    if cnt >= max:
+        split_index(index, elements, cnt)
+
+def split_index(index, elements, cnt):
+    elements.sort()
+    m = (cnt + 1) // 2
+    idx1 = make_index(max, elements[0:m])
+    idx2 = make_index(max, elements[m:cnt])
+    index['children'] = [idx1, idx2]
+    index['elements'] = None
+
 
 def print_index(index):
-    for item in index['elements']:
-        print item
+    if index['children']:
+        for idx in index['children']:
+            print '--'
+            print_index(idx)
+    else:
+        for item in index['elements']:
+            print item
 
-idx = make_index(100)
-for i in range(100):
+idx = make_index(10)
+for i in range(10):
     n = random.randint(1, 1000)
     add_to_index(idx, n)
 print_index(idx)
